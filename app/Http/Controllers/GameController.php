0c9d7313186744bcd2bb0024e03349887bdb1c53
fajaaro <fajarhamdani70@gmail.com>
2021-12-07 15:25:04 +0700

    layout

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -9,7 +9,7 @@ class GameController extends Controller
 {
 	public function index(Request $request)
 	{
-		if ($request->query) $games = Game::where('title', 'like', '%' . $request->query . '%')->latest()->paginate(8);
+		if ($request->q) $games = Game::where('title', 'like', '%' . $request->q . '%')->latest()->paginate(8);
 		else $games = Game::latest()->paginate(8);
 
         return view('games.index', compact('games'));
--- resources/views/games/index.blade.php
+++ resources/views/games/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-
+	<h1>{{ Request::query('q') ? 'Search Games' : 'Games' }}</h1>
 @endsection
