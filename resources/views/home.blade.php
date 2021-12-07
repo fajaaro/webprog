d49e61f9c0bd384cb57662715008b71eac9c499a
fajaaro <fajarhamdani70@gmail.com>
2021-12-07 17:37:55 +0700

    test from fajar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
                 <div class="card">
                     <div class="card-header">{{ __('Dashboard') }}</div>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nobis molestiae, laboriosam libero perspiciatis ipsa ab sed officia porro architecto esse ratione dolore magni ex labore consequuntur saepe odit ipsum?</p>
+                        <p>Fajar</p>
                     <div class="card-body">
                         @if (session('status'))
                             <div class="alert alert-success" role="alert">
