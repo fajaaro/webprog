6e43cf80f03f44d8f663a82fdbe3a8664cbb962d
fajaaroibs <fajar.hamdani@ib-synergy.co.id>
2022-01-08 06:16:43 +0700

    manage game admin

--- app/Http/Controllers/Admin/GameController.php
+++ app/Http/Controllers/Admin/GameController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Game;
+use App\Models\Genre;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -11,10 +12,17 @@ use Illuminate\Support\Str;
 
 class GameController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $games = Game::latest()->paginate(8);
-        return view('admin.games.index', compact('games'));
+        $genres = Genre::all();
+        $query = Game::latest();
+
+        if ($request->q) $query->where('title', 'like', '%' . $request->q . '%');
+        if ($request->genres) $query->whereIn('genre_id', $request->genres);
+
+        $games = $query->paginate(8);
+
+        return view('admin.games.index', compact('genres', 'games'));
     }
 
     public function create()
--- resources/views/admin/games/index.blade.php
+++ resources/views/admin/games/index.blade.php
@@ -1,95 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-  <div>
-    <h1>Manage Game</h1>
-    <h6>Filter by Game Name</h6>
-    <form class="">
-      <input class="form-control " type="search" placeholder="Search" aria-label="Search" style="width: 300px" > <br>
-      <h6>Filter by Game Category</h6>
-      <div class="row">
-        <div class="col-md-9">
-          <div class="row">
-            <div class="col-md-2">
-              <input type="checkbox" id="horror" name="horror" value="horror">
-              <label for="horror">Horror</label>
-            </div>
-            <div class="col-md-2">
-              <input type="checkbox" id="adventure" name="adventure" value="adventure">
-              <label for="adventure">Adventure</label>
-            </div>
-            <div class="col-md-2">
-              <input type="checkbox" id="action" name="action" value="action">
-              <label for="action">Action</label>
-            </div>
-            <div class="col-md-2">
-              <input type="checkbox" id="puzzle" name="puzzle" value="puzzle">
-              <label for="puzzle">Puzzle</label>
-            </div>
-            <div class="col-md-2">
-              <input type="checkbox" id="strategy" name="strategy" value="strategy">
-              <label for="strategy">Strategy</label>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-2">
-                <input type="checkbox" id="mmo" name="mmo" value="mmo">
-                <label for="roleplaying">Role-Playing</label>
-              </div>
-              <div class="col-md-2">
-                <input type="checkbox" id="simulation" name="simulation" value="simulation">
-                <label for="simulation">Simulation</label>
-              </div>
-              <div class="col-md-2">
-                  <input type="checkbox" id="sport" name="sport" value="sport">
-                  <label for="sport">Sport</label>
+    <div class="container-fluid">
+        <h1>Manage Game</h1>
+        <form action="{{ route('admin.games.index') }}" method="get">
+            <p>Filter by Game Name</p>
+            <input class="form-control w-25" type="search" placeholder="Search" aria-label="Search" name="q" value="{{ Request::query('q') }}">
+
+            <p class="mt-2">Filter by Game Category</p>
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row">
+                        @foreach($genres as $genre)
+                            <div class="col-md-2">
+                                <input type="checkbox" id="{{ $genre->name }}" name="genres[]" value="{{ $genre->id }}">
+                                <label for="{{ $genre->name }}">{{ $genre->name }}</label>
+                            </div>
+                        @endforeach
                 </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <button class="btn btn-outline-success px-2 mt-3" type="submit">Search</button>
-    </form>
-  </div>
-  <hr>
+            <button class="btn btn-outline-success px-2 mt-3" type="submit">Search</button>
+        </form>
 
-  <div class="row">
-    @forelse ($games as $game)
-        <div class="col-md-3 mb-3">
-            <div class="card shadow-sm">
-                <a href="{{ route('games.show', ['slug' => $game->slug]) }}">
-                    <img src="{{ str_starts_with($game->image_url, 'https') ? $game->image_url : Storage::url($game->image_url) }}" class="card-img-top" alt="game image">
-                </a>
-                <div class="card-body">
-                    <p class="card-text" style="font-weight: bold;">{{ $game->title }}</p>
-                    <p class="card-text">{{ $game->genre->name }}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="{{ route('games.show', ['slug' => $game->slug]) }}">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                            </a>
-                            <a href="{{ route('admin.games.edit', ['id' => $game->id]) }}">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                            </a>
+        <hr>
 
+        <div class="row">
+            @forelse ($games as $game)
+                <div class="col-md-3 mb-3">
+                    <div class="card shadow-sm">
+                        <a href="{{ route('games.show', ['slug' => $game->slug]) }}">
+                            <img src="{{ str_starts_with($game->image_url, 'https') ? $game->image_url : Storage::url($game->image_url) }}" class="card-img-top" alt="game image">
+                        </a>
+                        <div class="card-body">
+                            <p class="card-text" style="font-weight: bold;">{{ $game->title }}</p>
+                            <p class="card-text">{{ $game->genre->name }}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <a href="{{ route('games.show', ['slug' => $game->slug]) }}">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    </a>
+                                    <a href="{{ route('admin.games.edit', ['id' => $game->id]) }}">
+                                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    </a>
+
+                                </div>
+                                <small class="text-muted">{{ formatRupiah($game->price) }}</small>
+                            </div>
                         </div>
-                        <small class="text-muted">{{ formatRupiah($game->price) }}</small>
                     </div>
                 </div>
-            </div>
-        </div>
-    @empty
-        <div class="col">
-            <p>There are no games content can be showed right now</p>
-        </div>
-    @endforelse
-    @if ($games->lastPage() > 1)
-    <hr>
-@endif
+            @empty
+                <div class="col">
+                    <p>There are no games content can be showed right now</p>
+                </div>
+            @endforelse
+            @if ($games->lastPage() > 1)
+                <hr>
+            @endif
 
-{{ $games->links() }}
-</div>
+            {{ $games->links() }}
+        </div>
+    </div>
 
-<br><br>
 
 @endsection
